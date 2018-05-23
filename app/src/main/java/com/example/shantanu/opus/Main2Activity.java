a2389ab3d214eb6c95dda98ee529efd88337ec85
shan1322 <shaneyasingh13@gmail.com>
2018-05-23 22:44:07 +0530

    "With barcode"

--- app/src/main/java/com/example/shantanu/opus/Main2Activity.java
+++ app/src/main/java/com/example/shantanu/opus/Main2Activity.java
@@ -1,22 +1,41 @@
 package com.example.shantanu.opus;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
     private static final String TAG = "MainActivity";
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         findViewById(R.id.signout).setOnClickListener(this);
+        imageView=findViewById(R.id.imageView);
+        String text="sHANTANU" ;// Whatever you need to encode in the QR code
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
     private void signOut() {
         mAuth.signOut();
